2a8cfa08b8b3c5a2188eb94313fef38a5033f0fa
Majicmaj <52975556+majicmaj@users.noreply.github.com>
2020-05-21 13:50:38 -0400

    update functionallity

--- src/controllers/contact.js
+++ src/controllers/contact.js
@@ -9,5 +9,16 @@ module.exports = {
   readFromID: (req, res) =>
     Contact.find({ _id: req.params.id }).then(contact => res.json(contact)),
   readFromEmail: (req, res) =>
-    Contact.find({ email: req.params.email }).then(contact => res.json(contact))
+    Contact.find({ email: req.params.email }).then(contact =>
+      res.json(contact)
+    ),
+  // Update
+  updateFromID: (req, res) =>
+    Contact.findOneAndUpdate({ _id: req.params.id }.req.body).then(contact =>
+      res.json(contact)
+    ),
+  updateFromEmail: (req, res) =>
+    Contact.findOneAndUpdate({ email: req.params.email }.req.body).then(
+      contact => res.json(contact)
+    )
 };
