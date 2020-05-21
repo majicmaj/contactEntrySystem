2153e2fabb26a8be4f8127e506b92dcb215be559
Majicmaj <52975556+majicmaj@users.noreply.github.com>
2020-05-21 13:55:30 -0400

    added contact route with read functionality

---
+++ src/routes/contact.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+const contactController = require("../controllers/contact");
+
+// Read
+router.get("/", contactController.read);
+router.get("/id/:id", contactController.readFromID);
+router.get("/email/:email", contactController.readFromEmail);
