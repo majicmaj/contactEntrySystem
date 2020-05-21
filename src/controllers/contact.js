547e261acccc607bc40b90e0632de9dd2bfc3bcf
Majicmaj <52975556+majicmaj@users.noreply.github.com>
2020-05-21 13:50:19 -0400

    read functionality

--- src/controllers/contact.js
+++ src/controllers/contact.js
@@ -3,5 +3,11 @@ const Contact = require("../models/contact");
 module.exports = {
   // Create
   create: (req, res) =>
-    Contact.create(req.body).then(contact => res.json(contact))
+    Contact.create(req.body).then(contact => res.json(contact)),
+  // Read
+  read: (req, res) => Contact.find({}).then(contact => res.json(contact)),
+  readFromID: (req, res) =>
+    Contact.find({ _id: req.params.id }).then(contact => res.json(contact)),
+  readFromEmail: (req, res) =>
+    Contact.find({ email: req.params.email }).then(contact => res.json(contact))
 };
